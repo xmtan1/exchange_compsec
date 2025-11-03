5335bea971a8f4e79c2e4070f51e6922c56a1b46
huyhoang.ph <phungh67@gmail.com>
2025-11-03 20:23:56 +0100

    feat: spin up a simple TCP server

--- distributed-system/lab-1/http-server.go
+++ distributed-system/lab-1/http-server.go
@@ -4,27 +4,101 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"os/signal"
 	"sync"
+	"syscall"
+	"time"
 )
 
+const maxConnection = 10
+
 type server struct {
-	wg         sync.WaitGroup
-	listener   net.Listener
-	shutdonw   chan struct{}
-	connection chan net.Conn
+	connectionCount int
+	wg              sync.WaitGroup
+	listener        net.Listener
+	shutdown        chan struct{}
+	connection      chan net.Conn
 }
 
 func newServer(address string) (*server, error) {
 	listener, err := net.Listen("tcp", address)
 	if err != nil {
-		return nil, fmt.Errorf("Failed to listen on address %s : %w", address, err)
+		return nil, fmt.Errorf("failed to listen on address %s : %w", address, err)
 	}
 
 	return &server{
-		listener:   listener,
-		shutdonw:   make(chan struct{}),
-		connection: make(chan net.Conn),
+		connectionCount: 0,
+		listener:        listener,
+		shutdown:        make(chan struct{}),
+		connection:      make(chan net.Conn),
 	}, nil
+}
+
+// handle the "accept connection phase"
+func (s *server) acceptConnections() {
+	defer s.wg.Done()
+
+	for {
+		select {
+		case <-s.shutdown:
+			return
+		default:
+			conn, err := s.listener.Accept()
+			if err != nil {
+				continue
+			}
+			s.connection <- conn
+			s.connectionCount += 1
+		}
+	}
+}
+
+// pick a connection in "Connections" to serve
+func (s *server) handleConnections() {
+	defer s.wg.Done()
+
+	for {
+		select {
+		case <-s.shutdown:
+			return
+		case conn := <-s.connection:
+			go s.handleConnection(conn)
+		}
+	}
+}
+
+func (s *server) handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	fmt.Fprintf(conn, "TCP server accepted a connection, total connection: %d", s.connectionCount)
+	time.Sleep(5 * time.Second)
+	s.connectionCount -= 1
+	fmt.Fprintf(conn, "Handling connection completed, remaining %d", s.connectionCount)
+}
+
+func (s *server) Start() {
+	s.wg.Add(2)
+	go s.acceptConnections()
+	go s.handleConnections()
+}
+
+func (s *server) Stop() {
+	close(s.shutdown)
+	s.listener.Close()
+
+	done := make(chan struct{})
+	go func() {
+		s.wg.Wait()
+		close(done)
+	}()
+
+	select {
+	case <-done:
+		return
+	case <-time.After(time.Second):
+		fmt.Println("Time out waiting remaining connections to be finished.")
+		return
+	}
 }
 
 func main() {
@@ -40,4 +114,13 @@ func main() {
 		fmt.Println(error)
 		os.Exit(1)
 	}
+
+	s.Start()
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+	fmt.Println("Shutting down the server ...")
+	s.Stop()
+	fmt.Println("Server stopped.")
 }
