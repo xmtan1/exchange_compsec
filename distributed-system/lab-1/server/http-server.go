bdd49962e086688d456544467988885929283dbf
huyhoang.ph <phungh67@gmail.com>
2025-11-04 21:38:19 +0100

    feat: finish the code for a simple TCP server

--- distributed-system/lab-1/server/http-server.go
+++ distributed-system/lab-1/server/http-server.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"net/http"
 	"os"
 	"os/signal"
+	"path/filepath"
+	"strings"
 	"sync"
 	"sync/atomic"
 	"syscall"
@@ -14,12 +19,26 @@ import (
 
 const MAX_CONNECTION = 10
 
+var supportedTypes = map[string]string{
+	".html": "text/html",
+	".txt":  "text/plain",
+	".gif":  "image/gif",
+	".jpeg": "image/jpeg",
+	".jpg":  "image/jpeg",
+	".css":  "text/css",
+}
+
 type server struct {
+	// using atomic as a method for synchronization (to avoid race condition, package: sync/atomic)
 	connectionCount atomic.Int32
-	wg              sync.WaitGroup
-	listener        net.Listener
-	shutdown        chan struct{}
-	connection      chan net.Conn
+	// wait group for synchronization method, since this servers should handle connections concurrently (and also accept them as well)
+	wg sync.WaitGroup
+	// listener socket
+	listener net.Listener
+	// shutdown signal
+	shutdown chan struct{}
+	// connection pooling - no need for additional connection pool
+	connection chan net.Conn
 }
 
 func newServer(address string) (*server, error) {
@@ -75,12 +94,143 @@ func (s *server) handleConnection(conn net.Conn) {
 		s.connectionCount.Add(-1) // Decrement since we're closing early
 		return
 	}
-	fmt.Println("Accepted a connection")
 
-	time.Sleep(5 * time.Second)
+	fmt.Println("Accepted a connection")
+	reader := bufio.NewReader(conn)
+
+	req, err := http.ReadRequest(reader)
+
+	if err != nil {
+		log.Printf("Malformed request from the client")
+		s.handleError(conn, http.StatusBadRequest)
+		return
+	}
+
+	switch req.Method {
+	case "GET":
+		s.handleGet(conn, req)
+	case "POST":
+		s.handlePost(conn, req)
+	default:
+		s.handleError(conn, http.StatusNotImplemented)
+	}
 
 	remainingConn := s.connectionCount.Add(-1)
 	log.Printf("Handling a connection completed, remaining: %d", remainingConn)
+}
+
+func (s *server) handleGet(conn net.Conn, req *http.Request) {
+	const staticDir = "static"
+	path := filepath.Join(staticDir, filepath.Clean(req.URL.Path))
+
+	if !strings.HasPrefix(path, staticDir) {
+		s.handleError(conn, http.StatusBadRequest)
+		return
+	}
+
+	fileExt := filepath.Ext(path)
+	fileType, supported := supportedTypes[fileExt]
+	if !supported {
+		log.Printf("Bad request: unsupported extension %s", fileExt)
+		s.handleError(conn, http.StatusBadRequest)
+		return
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.Printf("Not found: %s", path)
+			s.handleError(conn, http.StatusNotFound)
+		} else {
+			log.Printf("Internal error: %v", err)
+			s.handleError(conn, http.StatusInternalServerError)
+		}
+		return
+	}
+
+	defer file.Close()
+
+	stat, err := file.Stat()
+	if err != nil {
+		log.Printf("Internal error while handling the file: %v", err)
+		s.handleError(conn, http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\n")
+	// Headers
+	fmt.Fprintf(conn, "Content-Type: %s\r\n", fileType)
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", stat.Size())
+	// End of headers (blank line)
+	fmt.Fprintf(conn, "\r\n")
+
+	if _, err := io.Copy(conn, file); err != nil {
+		log.Printf("Error while writing the file body: %v", err)
+	}
+}
+
+func (s *server) handlePost(conn net.Conn, req *http.Request) {
+	const uploadDir = "uploads"
+	if _, err := os.Stat(uploadDir); os.IsNotExist(err) {
+		if err := os.Mkdir(uploadDir, 0755); err != nil {
+			log.Printf("Internal error while creating storage: %v", err)
+			s.handleError(conn, http.StatusInternalServerError)
+			return
+		}
+	}
+
+	path := filepath.Join(uploadDir, filepath.Clean(req.URL.Path))
+
+	if !strings.HasPrefix(path, uploadDir) {
+		s.handleError(conn, http.StatusBadRequest)
+		return
+	}
+
+	file, err := os.Create(path)
+	if err != nil {
+		log.Printf("Internal error creating file: %v", err)
+		s.handleError(conn, http.StatusInternalServerError)
+		return
+	}
+
+	defer file.Close()
+
+	if _, err := io.Copy(file, req.Body); err != nil {
+		log.Printf("Internal error writing file: %v", err)
+		s.handleError(conn, http.StatusInternalServerError) // 500
+		return
+	}
+
+	log.Printf("File uploaded: %s", path)
+
+	// Status Line
+	fmt.Fprintf(conn, "HTTP/1.1 201 Created\r\n")
+
+	// Headers
+	fmt.Fprintf(conn, "Content-Type: text/plain\r\n")
+	// The 'Location' header tells the client where the new resource is
+	fmt.Fprintf(conn, "Location: %s\r\n", req.URL.Path)
+
+	// End of headers
+	fmt.Fprintf(conn, "\r\n")
+
+	// Body
+	fmt.Fprintln(conn, "File successfully uploaded.")
+
+}
+
+func (s *server) handleError(conn net.Conn, statusCode int) {
+	statusText := http.StatusText(statusCode)
+
+	// Status Line
+	fmt.Fprintf(conn, "HTTP/1.1 %d %s\r\n", statusCode, statusText)
+	// Headers
+	fmt.Fprintf(conn, "Content-Type: text/plain\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(statusText))
+	// End of headers
+	fmt.Fprintf(conn, "\r\n")
+	// Body
+	fmt.Fprintln(conn, statusText)
 }
 
 func (s *server) Start() {
